71e2a0a07ba394f6410b56d101b1ea01733dd599
sathvik-cb <sunnycanchi@gmail.com>
2022-03-26 12:51:26 +0530

    basic changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,17 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className="jumbotron text-center">
-            <h1>Company</h1>
-            <p>We specialize in blablabla</p>
-            <form className="form-inline">
+            <h1>We help famers cultivate new crops and help them increase profits!</h1>
+            <p>''Agricultural growth is a proven driver of poverty reduction. When agriculture stimulates growth ... the growth is twice as effective in reducing poverty as growth based in other sectors''
+- Giza Mdoe</p>
+            {/* <form className="form-inline">
                 <div className="input-group">
                     <input type="email" className="form-control" size="50" placeholder="Email Address" required />
                     <div className="input-group-btn">
                         <button type="button" className="btn btn-danger">Subscribe</button>
                     </div>
                 </div>
-            </form>
+            </form> */}
         </div>
     )
 }
