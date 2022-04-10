1ecf91aa2467b56e8ff64ff91b28b345fd1d0d68
gautham-c <gautham.chadalavada@gmail.com>
2022-04-10 11:02:44 +0530

    Updates

---
+++ precache-manifest.da4da0de7a5c75ad5ec05325a2d181f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921af68fce08571511b0f6266982e497",
+    "url": "/gautham-c.github.io/vekreate/index.html"
+  },
+  {
+    "revision": "9bf13b74ea0172cc26f3",
+    "url": "/gautham-c.github.io/vekreate/static/css/2.0f184c0a.chunk.css"
+  },
+  {
+    "revision": "dcd6c04594fd78d49a8d",
+    "url": "/gautham-c.github.io/vekreate/static/css/main.31aa14df.chunk.css"
+  },
+  {
+    "revision": "9bf13b74ea0172cc26f3",
+    "url": "/gautham-c.github.io/vekreate/static/js/2.32be941a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gautham-c.github.io/vekreate/static/js/2.32be941a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd6c04594fd78d49a8d",
+    "url": "/gautham-c.github.io/vekreate/static/js/main.26fd7366.chunk.js"
+  },
+  {
+    "revision": "59982e68be025a958537",
+    "url": "/gautham-c.github.io/vekreate/static/js/runtime-main.96daca5d.js"
+  },
+  {
+    "revision": "64e51230f0afd87262e731720a3fb7da",
+    "url": "/gautham-c.github.io/vekreate/static/media/background.64e51230.jpg"
+  },
+  {
+    "revision": "a5941c5e2e0243b264ecb671091890a6",
+    "url": "/gautham-c.github.io/vekreate/static/media/baler.a5941c5e.png"
+  },
+  {
+    "revision": "82da7631143821de6941b3162951332f",
+    "url": "/gautham-c.github.io/vekreate/static/media/farm.82da7631.png"
+  },
+  {
+    "revision": "8b22bf2b81a12ec563fdb6536d85d4fb",
+    "url": "/gautham-c.github.io/vekreate/static/media/paddy.8b22bf2b.png"
+  },
+  {
+    "revision": "2e452a8dfd9d1077a00243c888443650",
+    "url": "/gautham-c.github.io/vekreate/static/media/tractor.2e452a8d.png"
+  }
+]);
